4ac66ad2e84bfb364790bb0a4111eca479a63af4
Najir Hussain <business.najir@gmail.com>
2024-09-07 01:16:41 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -4,12 +4,13 @@
 2. Find the most common rating for movies and TV shows
 3. List all movies released in a specific year (e.g., 2020)
 4. Find the top 5 countries with the most content on Netflix
-5. Identify the longest movie or TV show duration
+5. Identify the longest movie
 6. Find content added in the last 5 years
 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
 8. List all TV shows with more than 5 seasons
 9. Count the number of content items in each genre
-10. Find the average release year for content produced in a specific country
+10.Find each year and the average numbers of content release in India on netflix. 
+return top 5 year with highest avg content release!
 11. List all movies that are documentaries
 12. Find all content without a director
 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
